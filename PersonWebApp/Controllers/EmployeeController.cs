d2f76864194fc0790d7ea31f2843e3a421bf0069
lbilde <larsbilde@gmail.com>
2016-09-06 14:58:04 +0200

    adding create forms

--- PersonWebApp/Controllers/EmployeeController.cs
+++ PersonWebApp/Controllers/EmployeeController.cs
@@ -24,5 +24,68 @@ namespace PersonWebApp.Controllers
             _em.Create(em);
             return RedirectToAction("Index");
         }
+
+
+        [ActionName("Single")]
+        public string Id(int id)
+        {
+            var person = _em.Get().FirstOrDefault(x => x.Id == id);
+            return person != null ? person.Id + " " + person.Name : "Not found";
+        }
+
+        [ActionName("First")]
+        public string GetFirst(int count)
+        {
+            if (count <= 0) return "Count must be above 0";
+            if (count > _em.Get().Count)
+            {
+                return GetStringPrint(_em.Get());
+            }
+            var persons = _em.Get().GetRange(0, count);
+            return GetStringPrint(persons);
+        }
+
+        [ActionName("Sorted")]
+        public string GetSorted(bool? desc)
+        {
+            if (desc.HasValue && desc.Value)
+            {
+                return GetStringPrint(_em.Get().OrderByDescending(x => x.Name).ToList());
+            }
+            return GetStringPrint(_em.Get().OrderBy(x => x.Name).ToList());
+        }
+
+        [ActionName("Paged")]
+        public string GetPaged(int page, int itemsPrPage, bool desc)
+        {
+            if (page <= 0 || itemsPrPage <= 0 || ((page - 1) * itemsPrPage) > _em.Get().Count)
+            {
+                return "Bad, page must be above 0 and itemsprpage must be above 0";
+            }
+            var pagedList = _em.Get()
+              .Skip((page - 1) * itemsPrPage)
+              .Take(itemsPrPage);
+            if (desc)
+            {
+                return GetStringPrint(pagedList.OrderByDescending(x => x.Name).ToList());
+            }
+            return GetStringPrint(pagedList.OrderBy(x => x.Name).ToList());
+        }
+
+
+
+        private string GetStringPrint(IEnumerable<Employee> empl)
+        {
+            var personList = "";
+            foreach (var person in empl)
+            {
+                personList += "<div style='overflow: hidden; white-space: nowrap; '>" +
+                        "<p style='color:darkblue'>" + person.Name + " - " + person.Id + "</p>" +
+                    "</div>";
+            }
+            return personList;
+        }
+
+
     }
 }
--- PersonWebApp/Controllers/PersonController.cs
+++ PersonWebApp/Controllers/PersonController.cs
@@ -13,91 +13,41 @@ namespace PersonWebApp.Controllers
         //Mem 1 PersonsList@111 <- Personcontroller@1
         //Mem 2 PersonsList@111 <- Personcontroller@2 John added
         //Mem 3 PersonsList@111 <- Personcontroller@3
+        private static int PersonId = 1 ;
 
         private static readonly List<Person> Persons =
             new List<Person>
-            {
-               new Person { Id = 1, Name = "Lars"},
-               new Person { Id = 2, Name = "Bob"},
-               new Person { Id = 3, Name = "Bill"},
-               new Person { Id = 4, Name = "Jane"},
-               new Person { Id = 5, Name = "Jill"},
-               new Person { Id = 6, Name = "Samuel"},
-               new Person { Id = 7, Name = "Jesica"}
-
+            {  new Person { Id = PersonId ++, Name = "Lars"},
+               new Person { Id = PersonId ++, Name = "Bob"},
             };
        // GET: Person
-        public string Index()
+        public ActionResult Index()
         {
-            return PrintPersons(Persons);
-        }
-        
-        [ActionName("Single")]
-        public string Id(int id)
-        {
-            var person = Persons.FirstOrDefault(x => x.Id == id);
-            return person != null ? person.Id + " " + person.Name : "Not found";
+            return View(Persons);
         }
 
-        [ActionName("First")]
-        public string GetFirst(int count)
+        public ActionResult Create()
         {
-            if (count <= 0) return "Count must be above 0";
-            if (count > Persons.Count)
-            {
-                return PrintPersons(Persons);
-            }
-            var persons = Persons.GetRange(0, count);
-            return PrintPersons(persons);
+            return View();
         }
 
-        [ActionName("Sorted")]
-        public string GetSorted(bool? desc)
+        [HttpPost]
+        public ActionResult Create(Person person)
         {
-            if (desc.HasValue && desc.Value)
-            {
-                return PrintPersons(Persons.OrderByDescending(x => x.Name).ToList());
-            }
-            return PrintPersons(Persons.OrderBy(x => x.Name).ToList());
+            person.Id = PersonId ++;
+            Persons.Add(person);
+            return RedirectToAction("Index");
         }
-
-        [ActionName("Paged")]
-        public string GetPaged(int page, int itemsPrPage, bool desc)
-        {
-            if (page <= 0 || itemsPrPage <= 0 || ((page - 1) * itemsPrPage) > Persons.Count)
-            {
-                return "Bad, page must be above 0 and itemsprpage must be above 0";
-            }
-            var pagedList = Persons
-              .Skip((page - 1) * itemsPrPage)
-              .Take(itemsPrPage);
-            if (desc)
-            {
-                return PrintPersons(pagedList.OrderByDescending(x => x.Name).ToList());
-            }
-            return PrintPersons(pagedList.OrderBy(x => x.Name).ToList());
-        }
-
-
 
         //Sending id ,Name
-        public string StoreData(Person person)
+        //Modelbinding
+        //Complex Modelbinding
+        public ActionResult StoreData(Person person)
         {
+            
             Persons.Add(person);
-            return PrintPersons(Persons);
+            return RedirectToAction("Index");
         }
 
-        
-        private string PrintPersons(IEnumerable<Person> persons )
-        {
-            var personList = "";
-            foreach (var person in persons)
-            {
-                personList += "<div style='overflow: hidden; white-space: nowrap; '>" +
-                        "<p style='color:darkblue'>" + person.Name + " - " + person.Id + "</p>"+
-                    "</div>";
-            }
-            return personList;
-        }
     }
 }
